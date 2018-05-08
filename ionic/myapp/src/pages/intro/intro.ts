0430357d4b72f974d2cfb94fbea0afcbcfa4d316
hudson <hudson@C3-LAB-PESQ-GRE.IASD.ORG>
2018-05-07 22:14:02 -0300

    videoaula de 16 à 24

--- ionic/myapp/src/pages/intro/intro.ts
+++ ionic/myapp/src/pages/intro/intro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the IntroPage page.
@@ -7,7 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-intro',
@@ -15,11 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
   }
 
+
+   goToTabsPage(){
+     this.navCtrl.push(TabsPage)
+  }
 }
