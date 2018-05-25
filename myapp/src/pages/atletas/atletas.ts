725c58629734c04886ca62e06fcc4c622ec87695
hudson <hudson@C3-LAB-PESQ-GRE.IASD.ORG>
2018-05-24 21:53:48 -0300

    videoaula 44

--- myapp/src/pages/atletas/atletas.ts
+++ myapp/src/pages/atletas/atletas.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartolaProvider } from '../../providers/cartola/cartola';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the AtletasPage page.
@@ -14,22 +15,41 @@ import { CartolaProvider } from '../../providers/cartola/cartola';
   selector: 'page-atletas',
   templateUrl: 'atletas.html',
   providers: [
-    CartolaProvider
+    CartolaProvider,
+    Camera
   ]
 })
 export class AtletasPage {
-
+  img = "";
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    private _cartolaProvider:CartolaProvider
+    private _cartolaProvider: CartolaProvider,
+    private camera: Camera
   ) {
   }
 
   ionViewDidLoad() {
-    this._cartolaProvider.atletas().subscribe(data=>{
+    this._cartolaProvider.atletas().subscribe(data => {
       console.log(data);
     })
+  }
+
+  tirarFoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
